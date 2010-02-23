dc2a87660939e746d4ee8f3c6756688d3d8945e2
frajasal <javier.santacruz.lc@gmail.com>
2010-02-23 17:41:40 +0000

    Pequeños arreglos de errores al escribir la configuración.
    git-svn-id: https://forja.rediris.es/svn/cusl4-idigin@158 9064d30b-c4ae-4e2d-b70c-096adb51a1d4

--- trunk/idg/opciones.py
+++ trunk/idg/opciones.py
@@ -12,7 +12,7 @@ class Opt(object):
     """@brief Establece las opciones básicas leyendo el config.xml"""
 
     ## Diccionario con las opciones por defecto
-    opts = { 'home' : './home',
+    _opts = { 'home' : './home',
             'share' : './share',
             'takuan' : '~/takuan',
             'bpelunit': '~/AeBpelEngine',
@@ -21,7 +21,7 @@ class Opt(object):
            }
 
     ## Diccionario con los atributos de las opciones por defecto
-    opts_nm = { 'home' : 'src',
+    _opts_nm = { 'home' : 'src',
             'share' : 'src',
             'takuan' : 'src',
             'bpelunit': 'src',
@@ -41,7 +41,7 @@ class Opt(object):
         @param nom El nombre de la opción.
         @retval El valor de la opción con nombre nom, o None si no existe.
         """
-        return self.opts[nom] if nom in self.opts else None
+        return self._opts[nom] if nom in self._opts else None
 
     def set(self, nom, val, attr="src"):
         """@brief Establece una opción.
@@ -51,12 +51,12 @@ class Opt(object):
         @retval True si se ha creado una opción nueva. False en otro caso.
         """
         # Añadirlo a las opciones si no estaba
-        retval = nom in self.opts
+        retval = nom in self._opts
         if not retval :
-            self.opts_nm[nom] = attr
+            self._opts_nm[nom] = attr
 
         # Establecer la opción
-        self.opts[nom] = val
+        self._opts[nom] = val
         return retval
 
     def write(self):
@@ -65,10 +65,10 @@ class Opt(object):
         existe, lo crea.
         """
 
-        config = path.join(self.opts['home'], 'config.xml')
+        config = self.config
         try:
             dom = et.ElementTree()
-            root = et.parse(config)
+            root = dom.parse(config)
         except:
             log.error(_("No se ha podido abrir para escritura de opciones: ") +
                       config)
@@ -76,19 +76,18 @@ class Opt(object):
         else:
             log.info(_("Escribiendo opciones config en: ") + config)
 
-        for nom, attr in self.opts_nm.iteritems() :
+        for nom, attr in self._opts_nm.iteritems() :
             e = root.find(nom)
             # Si no existe, crearlo
-            if not e :
+            if e is None:
                 e = et.SubElement(root, nom)
 
-            e.set(nom, attr, self.opts[nom])
+            e.set(attr, self._opts[nom])
 
         try:
             dom.write(config)
         except:
             log.error(_("No se han podido guardar las opciones en: ") + config)
-
 
     def read(self):
         """@brief Lee el fichero config."""
@@ -108,18 +107,27 @@ class Opt(object):
         for nom,attr in (('home', 'src'), ('share', 'src'),
                          ('takuan', 'src'),('bpelunit', 'src'),
                         ('svr', 'value'), ('port', 'value')):
-            try:
-                val = root.find(nom).get(attr)
-            except:
-                log.warning(_("Se usará el valor por defecto para: ") + nom)
-                val = self.opts[nom] 
-            finally:
-                # Expandir la ruta si es un src
-                if attr == 'src' :
-                    val = path.abspath(path.realpath(path.expanduser(val)))
-                    if not path.exists(val) :
-                        log.error(_("No se encuentra: ") + val)
 
-                self.opts[nom] = val
-                self.opts_nm[nom] = attr
+            # Buscar el elemento en el xml
+            e = root.find(nom)
+            if e is None or not e.get(attr):
+                log.warning(_("Se usará el valor por defecto para: ") + nom)
+                val = self._opts[nom] 
+            else :
+                val = e.get(attr)
+
+            # Expandir la ruta si es un src
+            if attr == 'src' :
+                val = path.abspath(path.realpath(path.expanduser(val)))
+                # Comprobar que la ruta existe
+                if not path.exists(val) :
+                    log.error(_("No se encuentra: ") + val)
+
+            # Guardar en el diccionario los valores.
+            self._opts[nom] = val
+            self._opts_nm[nom] = attr
+
+    def getall(self):
+        """@brief Devuelve todas las opciones disponibles. """
+        return self._opts
 
